c5fd00d8e2e95760d2d529816b42be27995a3886
IlyaSteik <ilya.steik@mail.ru>
2021-10-27 21:55:01 +0300

    f

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.03be6cc052340ae08db499648baff858.js"
+  "./precache-manifest.b09ddd42657178908b5d8c4661be09ae.js"
 );
 
 self.addEventListener('message', (event) => {
