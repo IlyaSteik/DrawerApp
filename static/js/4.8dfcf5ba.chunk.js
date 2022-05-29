e20d30034c5193fc7116fdfcf2f1f4af33d44b62
Uly <ilya.steik@mail.ru>
2022-05-29 20:49:23 +0300

    update

---
+++ static/js/4.8dfcf5ba.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpminiapp=window.webpackJsonpminiapp||[]).push([[4],{722:function(p,n,i){}}]);
+//# sourceMappingURL=4.8dfcf5ba.chunk.js.map
---
+++ upd/static/js/3.7d0f1b97.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpminiapp=window.webpackJsonpminiapp||[]).push([[3],{721:function(p,n,i){}}]);
+//# sourceMappingURL=3.7d0f1b97.chunk.js.map
